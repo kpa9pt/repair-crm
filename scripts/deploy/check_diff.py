5cdd3b371a9ab0b2a57155a005fd47b994579793
Igor Sinegubov <kpa9pter2@yandex.ru>
2026-06-24 23:51:54 +0300

    fix check_diff script

--- scripts/deploy/check_diff.py
+++ scripts/deploy/check_diff.py
@@ -1,33 +1,16 @@
 import json
 
-STATE_FILE = "state.json"
 
-
-def load_state():
-    with open(STATE_FILE, "r") as f:
+def load(path):
+    with open(path) as f:
         return json.load(f)
 
 
 def main():
-    state = load_state()
+    changes = load("images.json")
 
-    deploy_plan = []
+    deploy_plan = list(changes.keys())
 
-    for service_name, service in state["services"].items():
-
-        if service.get("strategy") != "blue-green":
-            continue
-
-        active = service["active"]
-        inactive = "green" if active == "blue" else "blue"
-
-        active_image = service[active]["image"]
-        inactive_image = service[inactive]["image"]
-
-        if active_image != inactive_image:
-            deploy_plan.append(service_name)
-
-    # Убираем indent=2, делаем компактный вывод
     print(json.dumps(deploy_plan))
 
 
