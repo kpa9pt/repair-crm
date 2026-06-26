34c35c6958e699b7567498d8f52db9cc4bf839a6
Igor Sinegubov <kpa9pter2@yandex.ru>
2026-06-27 02:07:42 +0300

    test deploy

--- services/gateway/app/main.py
+++ services/gateway/app/main.py
@@ -56,6 +56,6 @@ async def test_blue_green():
     return {"status": "ok"}
 
 
-# @app.get("/test_check_diff")
-# async def test_check_diff():
-#     return {"status": "ok"}
+@app.get("/test_check_diff")
+async def test_check_diff():
+    return {"status": "ok"}
